85a2a3948ade328ad54c4b1a7a2b7e71f2996cb7
lnuvy <hangeulgod@naver.com>
2022-02-21 18:55:26 +0900

    2월21일 첫 틀 잡기

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# @app.route('/home')
+# def home():
+#     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/mountainInfo')
+def mountain_info():
+    return render_template('mountainInfo.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+
+
---
+++ static/js/script.js
@@ -0,0 +1,3 @@
+
+// 테스트를 위한 border 스크립트
+$('#test').css('border', '1px solid tomato')
